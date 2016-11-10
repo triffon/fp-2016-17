6ba3493a6dc80e45af65ad16882df328e8568d2b
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-11-10 10:02:51 +0200

    Примери за работа с вложени списъци.

---
+++ nested_lists.scm
@@ -0,0 +1,44 @@
+(define (atom? x) (not (pair? x)))
+
+(define l '((1 (2)) (((3) 4) (5 (6)) () (7)) 8))
+
+(define (count-atoms l)
+  (cond ((null? l) 0)
+        ((atom? l) 1)
+        (else (+ (count-atoms (car l)) (count-atoms (cdr l))))))
+
+(define (flatten l)
+  (cond ((null? l) l)
+        ((atom? l) (list l))
+        (else (append (flatten (car l)) (flatten (cdr l))))))
+
+;(define (reverse l)
+;  (if (null? l) l
+;      (append (reverse (cdr l)) (list (car l)))))
+
+
+(define (deep-reverse l)
+  (cond ((null? l) l)
+        ((atom? l) l)
+        (else (append (deep-reverse (cdr l)) (list (deep-reverse (car l)))))))
+
+(define (deep-fold nv term op l)
+  (cond ((null? l) nv)
+        ((atom? l) (term l))
+        (else (op (deep-fold nv term op (car l))
+                  (deep-fold nv term op (cdr l))))))
+
+(define (foldr nv op l)
+  (if (null? l) nv
+      (op (car l) (foldr nv op (cdr l)))))
+
+(define (branch p? f g) (lambda (x) (if (p? x) (f x) (g x))))
+
+(define (deep-fold nv term op l)
+  (foldr nv op (map (branch null? (lambda (x) '())
+                    (branch atom? term (lambda (x) (deep-fold nv term op x)))) l)))
+
+
+(define (evali x) (eval x (interaction-environment)))
+
+
